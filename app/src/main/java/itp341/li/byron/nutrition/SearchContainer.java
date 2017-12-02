ce887af0badaa6639c7ef4d451524525e14318bb
ByronLi <byronli@usc.edu>
2017-12-01 16:43:34 -0800

    something broke

---
+++ app/src/main/java/itp341/li/byron/nutrition/SearchContainer.java
@@ -0,0 +1,13 @@
+package itp341.li.byron.nutrition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchContainer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_container);
+    }
+}
